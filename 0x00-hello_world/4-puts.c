9fefec2cac9fce9514d88f8fd929cf17567970fc
Eniola011 <yettyagbalu@gmail.com>
2023-04-20 19:30:57 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - main first entry point.
-*Return: return 0 always (success).
-*/
+ *main - main first entry point.
+ *
+ *Return: return 0 always (success).
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
