d5e225d1dc3fdede6cef8282e8c2b9c86451b0e7
Eniola011 <yettyagbalu@gmail.com>
2023-04-24 08:44:43 -0700

    iterate a number through for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - first entry point
+ *
+ *Return: should be 0 in this program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
