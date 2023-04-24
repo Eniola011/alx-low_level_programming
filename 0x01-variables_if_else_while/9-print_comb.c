a92243aa7a5c768c87d79a8c37ea15732c987e9c
Eniola011 <yettyagbalu@gmail.com>
2023-04-24 09:39:04 -0700

    Task 9-prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - first entry point
+ *
+ *Return: should be 0 in this program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
