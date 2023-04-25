6e4beef43265b9a4f45c046e845351f97bb35c99
Eniola011 <yettyagbalu@gmail.com>
2023-04-25 10:10:21 -0700

    Task 0: _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - starting point
+ *
+ *Return: should be 0 for this program
+ */
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
