275101dfd281189351906410ceaec12134387c70
Eniola011 <yettyagbalu@gmail.com>
2023-04-25 15:46:17 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	char j;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
