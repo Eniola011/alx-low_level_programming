246ff09957723d5ddeba2361a60119a289685e0a
Eniola011 <yettyagbalu@gmail.com>
2023-04-25 15:30:33 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints lower case letters 10 times
+ *
+ *Return: no value
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,4 @@
 Task 0: Write a program that prints _putchar, followed by a new line.
 Task 1: Write a function that prints the alphabet, in lowercase, followed by a new line.
+Task 2: Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+
