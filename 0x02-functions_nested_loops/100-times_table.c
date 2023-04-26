32045a0fefde76233baa3d506abf9c007c7848b3
Eniola011 <yettyagbalu@gmail.com>
2023-04-25 18:44:05 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,19 +25,19 @@ void print_times_table(int n)
 				{
 					_putchar(32);
 					_putchar(32);
-					_putchar(k + '0');
+					_putchar(k + 48);
 				}
 				else if (k <= 99)
 				{
 					_putchar(32);
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
 				}
 				else
 				{
-					_putchar(((k / 100) % 10) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(((k / 100) % 10) + 48);
+					_putchar(((k / 10) % 10) + 48);
+					_putchar((k % 10) + 48);
 				}
 			}
 			_putchar('\n');
