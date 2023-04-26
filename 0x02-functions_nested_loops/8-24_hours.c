e392739303b957cf230ff64b9faf740ccb819283
Eniola011 <yettyagbalu@gmail.com>
2023-04-25 17:42:28 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of day
+ *
+ * starting from 0:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int day;
+	int minut;
+
+	while (day < 24)
+	{
+		minut = 0;
+
+		while (minut < 60)
+		{
+			_putchar((day / 10) + '0');
+			_putchar((day % 10) + '0');
+			_putchar(':');
+			_putchar((minut / 10) + '0');
+			_putchar((minut % 10) + '0');
+			_putchar('\n');
+			minut++;
+		}
+		day++;
+	}
+}
