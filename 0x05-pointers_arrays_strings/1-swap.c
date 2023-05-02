3daff74298dcaa9b9f98e95a94236568b947d56e
Eniola011 <yettyagbalu@gmail.com>
2023-05-02 14:04:16 -0700

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - swap two integers
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: no return
+ */
+void swap_int(int *a, int *b)//a=98,b=42//
+{
+	*a = *a + *b;//a=140(98+42)//
+	*b = *a - *b;//b=98(140-42)//
+	*a = *a - *b;//a=42(140-98)//
+}
