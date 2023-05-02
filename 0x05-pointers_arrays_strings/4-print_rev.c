3123508c677dfc18d89a8707c013c8c61b2cc866
Eniola011 <yettyagbalu@gmail.com>
2023-05-02 15:06:08 -0700

    print strings in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string in reverse
+ *with a new line.
+ *@s: input string.
+ *
+ *Return: no return.
+ */
+void print_rev(char *s)
+{
+	int num = 0;
+
+	while (num >= 0)
+	{
+		if (s[num] == '\0')
+		{
+			break;
+		}
+		num++;
+	}
+	for (num--; num >= 0; num--)
+	{
+		_putchar(s[num]);
+	}
+	_putchar('\n');
+}
