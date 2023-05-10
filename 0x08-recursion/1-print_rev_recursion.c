21b271810342859257cbae96c6e1f1bd1569067b
Eniola011 <yettyagbalu@gmail.com>
2023-05-10 13:19:33 -0700

    print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *_print_rev_recursion - prints
@@ -11,7 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(++s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
