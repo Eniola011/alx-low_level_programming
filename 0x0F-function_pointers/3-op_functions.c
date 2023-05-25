063cd468d4eac6fe8613514a8af6e363cb16a6a1
Eniola011 <yettyagbalu@gmail.com>
2023-05-24 18:16:10 -0700

    Updated: 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,37 +2,54 @@
 
 /**
  * op_add - adds two integers
- * op_sub - ubtractstwo integers
- * op_mul - multiplies two integers
- * op_div - divides two integers
- * op_mod - modulos division of two int
  * @a: first integer
  * @b: second integer
- *
- * Return: the results from add,sub,
- *mul,div and mod
+ * Return: addition
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-/* subtraction */
+/**
+ *op_sub - subtracts two integers
+ *@a: first int
+ *@b: second int
+ *Return: subraction
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-/* multiplication */
+
+/**
+ *op_mul - multiplies two integers
+ *@a: first int
+ *@b: second int
+ *Return: multiplication
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/* division */
+
+/**
+ *op_div - divides two integers
+ *@a: first int
+ *@b: second int
+ *Return: division
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-/* remainder */
+
+/**
+ *op_mod - modulus division of two int
+ *@a: first int
+ *@b: second int
+ *Return: remainder
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
