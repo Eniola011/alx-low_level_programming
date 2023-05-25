3b412322c7fd08b7c55cc567b34debb3de4efcd0
Eniola011 <yettyagbalu@gmail.com>
2023-05-24 18:28:24 -0700

    updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,15 +17,9 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	res = get_op_func(argv[2]);
 
-	if (res == NULL)
+	if (!res)
 	{
 		printf("Error\n");
 		exit(99);
