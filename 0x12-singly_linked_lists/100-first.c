8831cdb310443f8dda14fecb174cf527a0356797
Eniola011 <yettyagbalu@gmail.com>
2023-06-01 19:09:14 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * first_main - first main before main
+ * Return: no value
+ */
+
+void __attribute__ ((constructor)) first_main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
