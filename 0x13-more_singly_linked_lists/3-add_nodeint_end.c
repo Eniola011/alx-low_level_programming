fac5ca88157890f53c603239783a38992c3efb0b
Eniola011 <yettyagbalu@gmail.com>
2023-06-06 13:56:14 -0700

    a function that adds a new node at the end of linkedlist

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds newnode at the end
+ * of linkedlist
+ * @head: starting point of linkedlist
+ * @n: integer
+ * Return: the address of the new element
+ * or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode;
+	listint_t *_temp;
+
+	newnode = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	_temp = *head;
+
+	if (*head == NULL)
+	{
+		newnode->next = NULL;
+		newnode->n = n;
+		*head = newnode;
+
+		return (newnode);
+	}
+
+	while (_temp->next != NULL)
+	{
+		_temp = _temp->next;
+	}
+
+	newnode->next = NULL;
+	newnode->n = n;
+	_temp->next = newnode;
+
+	return (newnode);
+}
