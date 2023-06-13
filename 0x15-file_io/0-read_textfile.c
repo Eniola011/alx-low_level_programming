c5978f556235f36d2621aeba78fb045fb3c37622
Eniola011 <yettyagbalu@gmail.com>
2023-06-13 13:05:05 -0700

    adjusted the comments in c file 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- * read_textfile: reads text files, prints letters
+/**
+ * read_textfile - reads text files, prints letters
  * @filename: name of file.
  * @letters: letters to be printed
  * Return: returns the actual number of letters it
  * could read and print
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected
- * amount of bytes,return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
