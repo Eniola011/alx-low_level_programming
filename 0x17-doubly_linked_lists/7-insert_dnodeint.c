9c3347a26f24c7b25673da3a43ca9aa4d0671153
Eniola011 <yettyagbalu@gmail.com>
2023-08-17 18:20:09 -0700

    adjusted 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,28 +1,21 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - a function that inserts a new node
- * at a given position.
+ * insert_dnodeint_at_index - a function that inserts a new node at a position.
  * @h: a double pointer head of doublylinked list(beginning point)
  * @idx: index of the list where the new node should be added.
- * Index starts at 0
  * @n: data that the list should contain
  * Return:  the address of the new node, or NULL if it failed
- * if it is not possible to add the new node at index idx, do not
- * add the new node and return NULL
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tempnode = *h, *newnode;
 	unsigned int itm; /* every node on the list */
 
-	/* allocate memory to the new node */
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 	{
 		return (NULL);
 	}
-	/* newnode points to the struct data and the value 'n'is stored in it */
 	newnode->n = n, newnode->prev = NULL, newnode->next = NULL;
 	if (idx == 0)
 	{
@@ -53,7 +46,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		tempnode->next->prev = newnode;
 	}
-
 	tempnode->next = newnode;
 	return (newnode);
 }
